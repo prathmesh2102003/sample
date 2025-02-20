4cdcdbb0d06a5c8bc3ef60d9627f50e815f27f1b
prathmesh2102003 <prathmeshamale646@gmail.com>
2025-02-20 10:05:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("index.html")
+
+@app.route("/videos/<filename>")
+def serve_video(filename):
+    return send_file(f"videos/{filename}", mimetype="video/mp4")
 
 if __name__ == "__main__":
     app.run(debug=True)
