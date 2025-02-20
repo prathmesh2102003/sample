e5a45fd71d11235796fb1e08b49166226f2c8b80
prathmesh2102003 <prathmeshamale646@gmail.com>
2025-02-20 10:23:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, render_template, send_file
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
-
-@app.route("/videos/<filename>")
-def serve_video(filename):
-    return send_file(f"videos/{filename}", mimetype="video/mp4")
+    return "Hello, GitHub Actions!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
